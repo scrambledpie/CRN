92017653d2a856894b1eb4df7b3d5b9e4a42e596
Michael Pearce <m.a.l.pearce@warwick.ac.uk>
2019-05-19 21:24:01 -0700

    gg

--- RUN_AMBULANCES.sh
+++ RUN_AMBULANCES.sh
@@ -4,8 +4,18 @@ Rscript Ambulances_runner.R $(($1+1))
 
 rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 
+
+Rscript Ambulances_runner.R $(($1+501))
+
+rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
+
+
 Rscript Ambulances_runner.R $(($1+1001))
 
 rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 
+
+Rscript Ambulances_runner.R $(($1+1501))
+
+rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 
