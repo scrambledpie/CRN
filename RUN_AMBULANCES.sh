c053afea462262d221d667b2ba1b24b483557136
Michael Pearce <m.a.l.pearce@warwick.ac.uk>
2019-05-19 21:05:36 -0700

    gg

--- RUN_AMBULANCES.sh
+++ RUN_AMBULANCES.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-Rscript Ambulances_runner.R $1
+Rscript Ambulances_runner.R $(($1+1))
 
 rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 
-Rscript Ambulances_runner.R $(($1+1000))
+Rscript Ambulances_runner.R $(($1+1001))
 
 rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 
--- RUN_ATO.sh
+++ RUN_ATO.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-Rscript ATO_runner.R $1
+Rscript ATO_runner.R $(($1 + 1))
 
 rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/ato/$2
 
-Rscript ATO_runner.R $(($1+1000))
+Rscript ATO_runner.R $(($1 + 1001))
 
 rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/ato/$2
 
