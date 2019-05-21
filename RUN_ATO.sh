299d675d5afd1423252a9a2454b9c275054a93f2
Michael Pearce <m.a.l.pearce@warwick.ac.uk>
2019-05-20 19:07:34 -0700

    full ATO

--- RUN_ATO.sh
+++ RUN_ATO.sh
@@ -2,10 +2,12 @@
 
 Rscript ATO_runner.R $(($1 + 1001))
 
-rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/ato/$2
+rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/ato/$2
+rsync -rv EachData1/ huanan:/home/michael/OPUS/ato/$2
 
 Rscript ATO_runner.R $(($1 + 1))
 
-rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/ato/$2
+rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/ato/$2
+rsync -rv EachData1/ huanan:/home/michael/OPUS/ato/$2
 
 
