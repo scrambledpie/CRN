04134dfff2686b3e81ab45d1c2d1bbc21de846e1
Michael Pearce <m.a.l.pearce@warwick.ac.uk>
2019-05-21 15:57:52 -0700

    ficed runners

--- RUN_AMBULANCES.sh
+++ RUN_AMBULANCES.sh
@@ -6,9 +6,5 @@ rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
 rsync -rv EachData1/ huanan:/home/michael/OPUS/amb/$2
 
 
-Rscript Ambulances_runner.R $(($1+801))
-
-rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/amb/$2
-rsync -rv EachData1/ huanan:/home/michael/OPUS/amb/$2
 
 
--- RUN_ATO.sh
+++ RUN_ATO.sh
@@ -1,9 +1,5 @@
 #/bin/bash
 
-Rscript ATO_runner.R $(($1 + 801))
-
-rsync -rv EachData1/ jamon:/storage/maths/phrnaj/opus/ato/$2
-rsync -rv EachData1/ huanan:/home/michael/OPUS/ato/$2
 
 Rscript ATO_runner.R $(($1 + 1))
 
