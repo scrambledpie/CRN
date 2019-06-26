ff39a07fa99fafe9f0ab776afff08217c6090708
Michael Pearce <m.a.l.pearce@warwick.ac.uk>
2019-06-26 09:50:01 -0700

    fff

--- RUN_DAILYAMBULANCES.sh
+++ RUN_DAILYAMBULANCES.sh
@@ -2,8 +2,8 @@
 
 Rscript DailyAmbulances_runner.R $(($1+1)) $2
 
-rsync -rv EachData1/ huanan:/home/michael/OPUS/amb/$3
-rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/amb/$3
+rsync -rv EachData1/ huanan:/home/michael/OPUS/damb/$3
+rsync -rv EachData1/ godzilla:/storage/maths/phrnaj/opus/damb/$3
 
 
 
